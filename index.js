e3e8221570461e20013ffe2d7568944af1551466
mher2323 <mardanyanmher2323@gmail.com>
2022-03-20 03:53:36 +0400

    new tasks add

--- index.js
+++ index.js
@@ -22,9 +22,40 @@ function replace2(a,b) {
     a = a - b
     return [a, b]
 }
+
+function strQuotes(str) {
+    return `hello ${str}`
+}
+
+function checkTheRange(age){
+    if (age >= 14 && age <= 90){
+        return true ;
+    }
+        return false ;
+}
+
+function toFixedNumber(num,n){
+    return num.toFixed(n)
+}
+
+function RoundNumber(num){
+    return num.toFixed(n)
+}
+
+function ucFirst(str) {
+    if (!str) return str;
+
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+
 module.exports = {
     reverseName,
     resign,
     replace,
-    replace2
+    replace2,
+    strQuotes,
+    checkTheRange,
+    toFixedNumber,
+    ucFirst
 }
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const {reverseName,resign,replace,replace2} = require('./index');
+const {reverseName,resign,replace,replace2,strQuotes,checkTheRange,toFixedNumber,ucFirst} = require('./index');
 
 describe('revertName function', ()=>{
     test('should be return name', () => {
@@ -31,6 +31,42 @@ describe('replace2 function', ()=>{
     test('should be return replace numbers', () => {
         const actual = replace2(7,9)
         const expected = [9,7] ;
+
+        expect(actual).toEqual(expected);
+    });
+})
+
+describe('strQuotes function', ()=>{
+    test('should be return hello str ', () => {
+        const actual = strQuotes('John')
+        const expected = 'hello John';
+
+        expect(actual).toEqual(expected);
+    });
+})
+
+describe('checkTheRange function', ()=>{
+    test('should be return true or false ', () => {
+        const actual = checkTheRange(30)
+        const expected = true ;
+
+        expect(actual).toEqual(expected);
+    });
+})
+
+describe('toFixedNumber function', ()=>{
+    test('should be return FixedNumber', () => {
+        const actual = toFixedNumber(12.35,1)
+        const expected = '12.3';
+
+        expect(actual).toEqual(expected);
+    });
+})
+
+describe('ucFirst function', ()=>{
+    test('should be return Upper Case first symbol', () => {
+        const actual = ucFirst("john")
+        const expected = 'John';
 
         expect(actual).toEqual(expected);
     });
